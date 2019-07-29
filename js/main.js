ba96d86be4e576c24bd3451a585d875c70210a76
Juan Espinosa <arq.jorrin@gmail.com>
2019-07-29 15:21:53 +1000

    Add interactive greeting

---
+++ js/main.js
@@ -0,0 +1,44 @@
+// The changingGreeting function will start
+// working before the page is completely loaded,
+// toggling the greeting message in 4 different languages,
+// depending the time that the user visits the website.
+
+const changingGreeting = () => {
+
+   const greetings = {
+      english: ['Good morning', 'Good afternoon', 'Good evening'],
+      spanish: ['Buenos di&#769as', 'Buenas tardes', 'Buenas tardes'],
+      french: ['Bonjour', 'Bonjour', 'Bonsoir'],
+      slovak: ['Dobre&#769 ra&#769no', 'Dobry&#769 de&#328', 'Dobry&#769 de&#328']
+   };
+
+   // I added the letters 'í', 'é', 'ý' and 'ň' with
+   // the HTML entities 'i&#769', 'e&#769', 'y&#769' and
+   // '&#328' respectively
+
+   const allLanguages = ['english', 'spanish', 'french', 'slovak'];
+
+   let language = 0;
+
+   let interval = setInterval(() => {
+      if (language <= 3) {
+         $('#greeting').html(`${greetings[allLanguages[language]][0]},`);
+         language++;
+      }
+      else {
+         language = 0;
+      }
+   }, 2500);
+
+};
+
+changingGreeting();
+
+// This sections is the JavaScript which will be working
+// after the website is completely loaded.
+
+$(document).ready(() => {
+
+console.log('The page is ready to load the rest of javascript');
+
+});
